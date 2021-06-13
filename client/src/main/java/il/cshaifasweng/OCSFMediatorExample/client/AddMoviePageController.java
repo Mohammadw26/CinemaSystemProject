f93bb2d49e80abc51643c628c138e1d27a280a33
Mohammad <Mohammadw996@gmail.com>
2021-06-13 17:48:24 +0300

    add movie page

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/AddMoviePageController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/AddMoviePageController.java
@@ -2,9 +2,14 @@ package il.cshaifasweng.OCSFMediatorExample.client;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 
 public class AddMoviePageController {
 
@@ -27,16 +32,77 @@ public class AddMoviePageController {
     private TextField producersField;
 
     @FXML
-    private TextField costField;
-
-    @FXML
     private TextField actorsField;
 
     @FXML
     private TextField urlField;
 
     @FXML
-    private ImageView movieImage;
+    private ImageView showImage;
+
+    @FXML
+    private Label showProducers;
+
+    @FXML
+    private Label showActors;
+
+    @FXML
+    private Label showEngTitle;
+
+    @FXML
+    private Label showHebTitle;
+
+    @FXML
+    private Button approveBtn;
+
+    @FXML
+    private Button cancelBtn;
+
+    @FXML
+    void addMovieAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void returnToCatalog(ActionEvent event) {
+
+    }
+
+    @FXML
+    void setActors(KeyEvent event) {
+    	showActors.setText(actorsField.getText());
+
+    }
+
+    @FXML
+    void setDescription(KeyEvent event) {
+
+    }
+
+    @FXML
+    void setEngTitle(KeyEvent event) {
+    	showEngTitle.setText(engMovieTitle.getText());
+    }
+
+    @FXML
+    void setHebTitle(KeyEvent event) {
+    	showHebTitle.setText(hebMovieTitle.getText());
+
+
+    }
+
+    @FXML
+    void setImageURL(KeyEvent event) {
+    	Image image = new Image(urlField.getText());
+    	showImage.setImage(image);
+    	showImage.setPreserveRatio(false);
+
+    }
+
+    @FXML
+    void setProducers(KeyEvent event) {
+    	showProducers.setText(producersField.getText());
+    }
 
     @FXML
     void initialize() {
@@ -44,10 +110,15 @@ public class AddMoviePageController {
         assert hebMovieTitle != null : "fx:id=\"hebMovieTitle\" was not injected: check your FXML file 'addMoviePage.fxml'.";
         assert descriptionField != null : "fx:id=\"descriptionField\" was not injected: check your FXML file 'addMoviePage.fxml'.";
         assert producersField != null : "fx:id=\"producersField\" was not injected: check your FXML file 'addMoviePage.fxml'.";
-        assert costField != null : "fx:id=\"costField\" was not injected: check your FXML file 'addMoviePage.fxml'.";
         assert actorsField != null : "fx:id=\"actorsField\" was not injected: check your FXML file 'addMoviePage.fxml'.";
         assert urlField != null : "fx:id=\"urlField\" was not injected: check your FXML file 'addMoviePage.fxml'.";
-        assert movieImage != null : "fx:id=\"movieImage\" was not injected: check your FXML file 'addMoviePage.fxml'.";
+        assert showImage != null : "fx:id=\"showImage\" was not injected: check your FXML file 'addMoviePage.fxml'.";
+        assert showProducers != null : "fx:id=\"showProducers\" was not injected: check your FXML file 'addMoviePage.fxml'.";
+        assert showActors != null : "fx:id=\"showActors\" was not injected: check your FXML file 'addMoviePage.fxml'.";
+        assert showEngTitle != null : "fx:id=\"showEngTitle\" was not injected: check your FXML file 'addMoviePage.fxml'.";
+        assert showHebTitle != null : "fx:id=\"showHebTitle\" was not injected: check your FXML file 'addMoviePage.fxml'.";
+        assert approveBtn != null : "fx:id=\"approveBtn\" was not injected: check your FXML file 'addMoviePage.fxml'.";
+        assert cancelBtn != null : "fx:id=\"cancelBtn\" was not injected: check your FXML file 'addMoviePage.fxml'.";
 
     }
 }
