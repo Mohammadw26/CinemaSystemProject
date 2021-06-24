d777bc0c6823460c05c70fe98389d6d8d39e2cba
EliasShalloufi <eliaso_sh@hotmail.com>
2021-06-24 20:50:48 +0300

    tabs added

---
+++ entities/src/main/java/il/cshaifasweng/OCSFMediatorExample/entities/TabPurchase.java
@@ -0,0 +1,24 @@
+package il.cshaifasweng.OCSFMediatorExample.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ticketTabs")
+public class TabPurchase extends Purchase {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public TabPurchase() {
+		this.cost = 600;
+	}
+	
+	public TabPurchase(long cridNum, CinemaMember member) {
+		this.cost = 600;
+		this.creditCardNum = cridNum;
+		this.customer = member;
+	}
+}
